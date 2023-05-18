afaefd8ff72214b871a02409800e5eea3d06ec5f
ambrose-kibet <ambrosekibet36@gmail.com>
2023-05-18 13:05:22 +0300

    fix linters

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-belongs_to :user
-belongs_to :post
+  belongs_to :user
+  belongs_to :post
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-belongs_to :user
-has_many :likes
-has_many :comments
+  belongs_to :user
+  has_many :likes
+  has_many :comments
 end
