b71fcc8ea6f122a5fb0837256b9b35ea67eaf1e2
ambrose-kibet <ambrosekibet36@gmail.com>
2023-05-18 11:40:56 +0300

    add likes references

--- db/migrate/20230518083912_addrefenecelikes.rb
+++ db/migrate/20230518083912_addrefenecelikes.rb
@@ -1,4 +1,6 @@
 class Addrefenecelikes < ActiveRecord::Migration[7.0]
   def change
+    add_reference(:likes, :user, foreign_key: { to_table: :users })
+    add_reference(:likes, :post, foreign_key: { to_table: :post })
   end
 end
