8fdf2327a4b8159a85cd29507153f4067878262e
ambrose-kibet <ambrosekibet36@gmail.com>
2023-05-18 11:47:55 +0300

    run migrations

--- db/migrate/20230518083912_addrefenecelikes.rb
+++ db/migrate/20230518083912_addrefenecelikes.rb
@@ -1,6 +1,6 @@
 class Addrefenecelikes < ActiveRecord::Migration[7.0]
   def change
     add_reference(:likes, :user, foreign_key: { to_table: :users })
-    add_reference(:likes, :post, foreign_key: { to_table: :post })
+    add_reference(:likes, :post, foreign_key: { to_table: :posts })
   end
 end
--- db/migrate/20230518084327_addusereferencetocomments.rb
+++ db/migrate/20230518084327_addusereferencetocomments.rb
@@ -1,6 +1,6 @@
 class Addusereferencetocomments < ActiveRecord::Migration[7.0]
   def change
-        add_reference(:likes, :user, foreign_key: { to_table: :users })
-        add_reference(:likes, :post, foreign_key: { to_table: :post })
+        add_reference(:comments, :user, foreign_key: { to_table: :users })
+        add_reference(:comments, :post, foreign_key: { to_table: :posts })
   end
 end
