ecbbc47298cdfb9c641b45f5742fd0e8bb48f5a2
ambrose-kibet <ambrosekibet36@gmail.com>
2023-05-18 13:40:56 +0300

    add modes plus methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,15 @@ class User < ApplicationRecord
   belongs_to :user
   has_many :likes
   has_many :comments
+
+  after_create :update_user_posts_counter
+  after_destroy :update_user_posts_counter
+
+  def update_user_posts_counter
+    user.update(posts_counter: user.posts.count)
+  end
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
   has_many :posts
   has_many :likes
   has_many :comments
+
+
+  def recent_posts
+    posts.first(3)
+  end
 end
