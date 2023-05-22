d361c4341f35496b4937723576bc27f1b6231492
ambrose-kibet <ambrosekibet36@gmail.com>
2023-05-22 12:42:37 +0300

    add controller actions and views

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,18 @@
+class PostsController < ApplicationController
+  before_action :set_user
+
+  def index
+    @articles = @user.posts
+  end
+
+  def show
+    @article = @user.posts.find(params[:id])
+    
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+end
