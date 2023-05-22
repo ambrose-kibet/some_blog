f6dfaf013d87b5b76b9883beede01518f992a446
ambrose-kibet <ambrosekibet36@gmail.com>
2023-05-22 18:25:06 +0300

    grouped tests

--- spec/requests/posts_request_spec.rb
+++ spec/requests/posts_request_spec.rb
@@ -1,29 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  it 'should succesfully request for posts ' do
-    get '/users/13/posts'
-    expect(response.status).to eq(200)
+  describe 'GET /index' do
+    it 'should succesfully request for posts ' do
+      get '/users/13/posts'
+      expect(response.status).to eq(200)
+    end
+    it 'should succesfully render index template ' do
+      get '/users/13/posts'
+      expect(response.status).to render_template(:index)
+    end
+    it 'check the response body includes correct placeholder text' do
+      get '/users/13/posts'
+      expect(response.body).to include('<h1>this is the place holder for posts</h1>')
+    end
   end
-  it 'should succesfully render index template ' do
-    get '/users/13/posts'
-    expect(response.status).to render_template(:index)
-  end
-  it 'check the response body includes correct placeholder text' do
-    get '/users/13/posts'
-    expect(response.body).to include('<h1>this is the place holder for posts</h1>')
-  end
-  it 'should succesfully render show template ' do
-    get '/users/13/posts/1'
-    expect(response.status).to render_template(:show)
-  end
-  it 'should succesfully request for posts ' do
-    get '/users/13/posts/1'
-    expect(response.status).to eq(200)
-  end
+  describe 'GET /show' do
+    it 'should succesfully render show template ' do
+      get '/users/13/posts/1'
+      expect(response.status).to render_template(:show)
+    end
+    it 'should succesfully request for posts ' do
+      get '/users/13/posts/1'
+      expect(response.status).to eq(200)
+    end
 
-  it 'check the response body includes correct placeholder text' do
-    get '/users/13/posts/1'
-    expect(response.body).to include('<h1>placeholder for show template</h1>')
+    it 'check the response body includes correct placeholder text' do
+      get '/users/13/posts/1'
+      expect(response.body).to include('<h1>placeholder for show template</h1>')
+    end
   end
 end
--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,28 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  it 'should succesfully request for users ' do
-    get '/users/'
-    expect(response.status).to eq(200)
+  describe 'GET /index' do
+    it 'should succesfully request for users ' do
+      get '/users/'
+      expect(response.status).to eq(200)
+    end
+    it 'should succesfully render index template ' do
+      get '/users/'
+      expect(response.status).to render_template(:index)
+    end
+    it 'check the response body includes correct placeholder text' do
+      get '/users/'
+      expect(response.body).to include('<h1>All users place holder</h1>')
+    end
   end
-  it 'should succesfully render index template ' do
-    get '/users/'
-    expect(response.status).to render_template(:index)
-  end
-  it 'check the response body includes correct placeholder text' do
-    get '/users/'
-    expect(response.body).to include('<h1>All users place holder</h1>')
-  end
-  it 'should succesfully render show template ' do
-    get '/users/1'
-    expect(response.status).to render_template(:show)
-  end
-  it 'should succesfully request for users ' do
-    get '/users/1'
-    expect(response.status).to eq(200)
-  end
-  it 'check the response body includes correct placeholder text' do
-    get '/users/1'
-    expect(response.body).to include('<h1>user show placeholder</h1>')
+  describe 'GET /show' do
+    it 'should succesfully render show template ' do
+      get '/users/1'
+      expect(response.status).to render_template(:show)
+    end
+    it 'should succesfully request for users ' do
+      get '/users/1'
+      expect(response.status).to eq(200)
+    end
+    it 'check the response body includes correct placeholder text' do
+      get '/users/1'
+      expect(response.body).to include('<h1>user show placeholder</h1>')
+    end
   end
 end
