23514be3082a511a774db31d4c756162efde40bc
ambrose-kibet <ambrosekibet36@gmail.com>
2023-05-31 13:46:57 +0300

    sign out

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :members
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+
   # Defines the root path route ("/")
   root 'users#index'
   resources :users, only: [:index,:show] do
